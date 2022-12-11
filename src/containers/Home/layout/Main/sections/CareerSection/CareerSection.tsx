50664c1c8a97a14e59f901e305c3dfbc9b8e9ce7
iamnayeon <nana.nayeonk@gmail.com>
2022-12-11 15:44:20 +0900

    fix: fix wrong word

--- src/containers/Home/layout/Main/sections/CareerSection/CareerSection.tsx
+++ src/containers/Home/layout/Main/sections/CareerSection/CareerSection.tsx
@@ -11,7 +11,7 @@ const careerItems: ICareerItem[] = [
   {
     title: "비프로컴퍼니 (Bepro UK-LTD)",
     description:
-      "누적 경기 분석 횟수 3만 건, 전 세계 40여 개 국가, 2,000여 개의 팀이 이용하는 축구 영상, 데이터 분석 서비스를 제공하는 시리즈A 스타트업",
+      "누적 경기 분석 횟수 3만 건, 전 세계 40여 개 국가, 2,000여 개의 팀이 이용하는 축구 영상, 데이터 분석 서비스를 제공하는 시리즈C 스타트업",
     position: "Frontend Developer",
     duration: "2020.09 ~ 2022.08 (2년)",
   },
